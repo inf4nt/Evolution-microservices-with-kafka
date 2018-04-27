4afcfdb161e5b3665222e91e0fcfb45bd0a8f5b8
Infant <com.infant@gmail.com>
2018-04-28 02:53:59 +0300

    stable build

---
+++ EV-DIRECT-MESSAGE/src/main/java/com/evolution/direct/message/topology/core/MessageCreateEvent.java
@@ -0,0 +1,33 @@
+package com.evolution.direct.message.topology.core;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.Wither;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Builder
+@Wither
+public class MessageCreateEvent implements MessageEvent {
+
+    @NotEmpty
+    String key;
+
+    @NotEmpty
+    String eventNumber;
+
+    @NotEmpty
+    String text;
+
+    @NotEmpty
+    String sender;
+
+    @NotEmpty
+    String recipient;
+}
---
+++ EV-DIRECT-MESSAGE/src/main/java/com/evolution/direct/message/topology/core/MessageEvent.java
@@ -0,0 +1,6 @@
+package com.evolution.direct.message.topology.core;
+
+import com.evolution.direct.message.core.Event;
+
+public interface MessageEvent extends Event<String, String> {
+}
