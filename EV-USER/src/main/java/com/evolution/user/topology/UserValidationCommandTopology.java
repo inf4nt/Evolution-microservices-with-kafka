6d5dc3bbc0c8226c2dde170eb4c693483482da3a
Infant <com.infant@gmail.com>
2018-04-30 18:48:54 +0300

    command feed

--- EV-USER/src/main/java/com/evolution/user/topology/UserValidationCommandTopology.java
+++ EV-USER/src/main/java/com/evolution/user/topology/UserValidationCommandTopology.java
@@ -14,15 +14,13 @@ import org.apache.kafka.streams.Consumed;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.Joined;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.KTable;
-import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.support.serializer.JsonSerde;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 import static com.evolution.user.core.HelpService.getFeed;
 
@@ -46,6 +44,7 @@ public class UserValidationCommandTopology extends AbstractTopology {
         Serde<UserCreateCommand> userCreateCommandSerde = new JsonSerde<>(UserCreateCommand.class, objectMapper);
         Serde<UserUsernameKey> userStateKeyUsernameSerde = new JsonSerde<>(UserUsernameKey.class, objectMapper);
         Serde<AbstractCommand> abstractCommandSerde = new JsonSerde<>(AbstractCommand.class, objectMapper);
+        Serde<CommandExecute> commandExecuteSerde = new JsonSerde<>(CommandExecute.class, objectMapper);
 
         final KStream<String, UserCreateCommand> userCreateCommandKStream = builder
                 .stream(getFeed(UserCreateCommand.class), Consumed.with(Serdes.String(), userCreateCommandSerde));
@@ -53,48 +52,78 @@ public class UserValidationCommandTopology extends AbstractTopology {
         final KTable<String, UserUsernameKey> userStateKeyUsernameKTable = builder
                 .table(getFeed(UserUsernameKey.class), Consumed.with(Serdes.String(), userStateKeyUsernameSerde));
 
-        final KStream<String, UserCreateCommandValidationResponse> userCreateCommandValidationResponseKStream =
+        final KStream<String, CommandExecute> commandExecuteKStream =
                 userCreateCommandKStream
                         .selectKey((k, v) -> v.getUsername())
-                        .leftJoin(userStateKeyUsernameKTable, (uc, us) -> UserCreateCommandValidationResponse
+                        .leftJoin(userStateKeyUsernameKTable, (uc, us) -> CommandExecute
                                         .builder()
-                                        .errors(us == null ? new ArrayList<>() : new ArrayList<CommandErrors>() {{
-                                            add(CommandErrors.USERNAME_NOT_UNIQUE);
+                                        .errors(us == null ? new ArrayList<>() : new ArrayList<String>() {{
+                                            add(CommandErrors.USERNAME_NOT_UNIQUE.name());
                                         }})
                                         .key(uc.getKey())
                                         .operationNumber(uc.getOperationNumber())
-                                        .userCreateCommand(UserCreateCommand.builder()
-                                                .key(uc.getKey())
-                                                .username(uc.getUsername())
-                                                .password(uc.getPassword())
-                                                .firstName(uc.getFirstName())
-                                                .lastName(uc.getLastName())
-                                                .operationNumber(uc.getOperationNumber())
-                                                .build())
                                         .build(),
-                                Joined.with(Serdes.String(), userCreateCommandSerde, userStateKeyUsernameSerde));
+                                Joined.with(Serdes.String(), userCreateCommandSerde, userStateKeyUsernameSerde))
+                .map((k, v) -> new KeyValue<>(v.getKey(), v));
 
-        userCreateCommandValidationResponseKStream
-                .to(getFeed(UserCreateCommandValidationResponse.class), Produced.with(Serdes.String(), userCreateCommandValidationResponseSerde));
-
-        final KStream<String, AbstractCommand> abstractCommandKStream = userCreateCommandValidationResponseKStream
-                .map((k, v) -> new KeyValue<>(k, AbstractCommand.builder()
-                        .key(k)
-                        .operationNumber(v.getOperationNumber())
-                        .errors(v.getErrors())
-                        .build()));
-        abstractCommandKStream.to(getFeed(AbstractCommand.class), Produced.with(Serdes.String(), abstractCommandSerde));
+        commandExecuteKStream
+                .to(getFeed(CommandExecute.class), Produced.with(Serdes.String(), commandExecuteSerde));
 
 
-        final KStream<String, UserCreateEvent> userCreateEventKStream = userCreateCommandValidationResponseKStream
+        final KStream<String, UserCreateEvent> userCreateEventKStream = commandExecuteKStream
                 .filter((k, v) -> v.getErrors().isEmpty())
-                .map((k, v) -> new KeyValue<>(k, UserCreateEvent.builder()
-                        .key(v.getKey())
-                        .eventNumber(v.getOperationNumber())
-                        .userCreateCommand(v.getUserCreateCommand())
-                        .build()));
+                .join(userCreateCommandKStream, (ce, uc) -> UserCreateEvent.builder()
+                                .key(ce.getKey())
+                                .eventNumber(ce.getOperationNumber())
+                                .userCreateCommand(uc)
+                                .build(),
+                        JoinWindows.of(TimeUnit.MINUTES.toMillis(5)),
+                        Joined.with(Serdes.String(), commandExecuteSerde, userCreateCommandSerde));
 
         userCreateEventKStream.to(getFeed(UserCreateEvent.class), Produced.with(Serdes.String(), userCreateEventSerde));
+
+//        final KStream<String, UserCreateCommandValidationResponse> userCreateCommandValidationResponseKStream =
+//                userCreateCommandKStream
+//                        .selectKey((k, v) -> v.getUsername())
+//                        .leftJoin(userStateKeyUsernameKTable, (uc, us) -> UserCreateCommandValidationResponse
+//                                        .builder()
+//                                        .errors(us == null ? new ArrayList<>() : new ArrayList<CommandErrors>() {{
+//                                            add(CommandErrors.USERNAME_NOT_UNIQUE);
+//                                        }})
+//                                        .key(uc.getKey())
+//                                        .operationNumber(uc.getOperationNumber())
+//                                        .userCreateCommand(UserCreateCommand.builder()
+//                                                .key(uc.getKey())
+//                                                .username(uc.getUsername())
+//                                                .password(uc.getPassword())
+//                                                .firstName(uc.getFirstName())
+//                                                .lastName(uc.getLastName())
+//                                                .operationNumber(uc.getOperationNumber())
+//                                                .build())
+//                                        .build(),
+//                                Joined.with(Serdes.String(), userCreateCommandSerde, userStateKeyUsernameSerde));
+//
+//        userCreateCommandValidationResponseKStream
+//                .to(getFeed(UserCreateCommandValidationResponse.class), Produced.with(Serdes.String(), userCreateCommandValidationResponseSerde));
+//
+//        final KStream<String, AbstractCommand> abstractCommandKStream = userCreateCommandValidationResponseKStream
+//                .map((k, v) -> new KeyValue<>(k, AbstractCommand.builder()
+//                        .key(k)
+//                        .operationNumber(v.getOperationNumber())
+//                        .errors(v.getErrors())
+//                        .build()));
+//        abstractCommandKStream.to(getFeed(AbstractCommand.class), Produced.with(Serdes.String(), abstractCommandSerde));
+
+
+//        final KStream<String, UserCreateEvent> userCreateEventKStream = userCreateCommandValidationResponseKStream
+//                .filter((k, v) -> v.getErrors().isEmpty())
+//                .map((k, v) -> new KeyValue<>(k, UserCreateEvent.builder()
+//                        .key(v.getKey())
+//                        .eventNumber(v.getOperationNumber())
+//                        .userCreateCommand(v.getUserCreateCommand())
+//                        .build()));
+//
+//        userCreateEventKStream.to(getFeed(UserCreateEvent.class), Produced.with(Serdes.String(), userCreateEventSerde));
 
         KafkaStreams streams = new KafkaStreams(builder.build(), streamsConfig());
         streams.start();
